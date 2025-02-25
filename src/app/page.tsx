c5e96c6cdb9b84c8bda8ff3c8dc828f26918310a
rnbrk02 <rayan.barki.rayan@gmail.com>
2025-02-25 12:09:00 +0300

    Описание изменений

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
-      <div className="max-w-lg w-full bg-white shadow-lg rounded-2xl p-6">
-        <h1 className="text-2xl font-bold text-gray-900">Привет! Я Раян</h1>
-        <p className="mt-2 text-gray-700">
-        Люблю технологии, нестандартные задачи и запах свежесозданных проектов. 🚀 </p>
+      <div className="max-w-lg w-full bg-white shadow-lg rounded-2xl p-6 text-center">
+        {/* Заголовок по центру */}
+        <h1 className="text-2xl font-bold text-gray-1000">Привет! Я Раян</h1>
 
-        {/* Фото */}
-        <img
-          src="/profile.jpg" 
-          alt="Евгений Кравцов"
-          className="w-32 h-32 rounded-lg mt-4"
-        />
+        <div className="grid grid-cols-2 gap-100 items-center mt-6 text-center">
+          {/* Текст слева */}
+          <p className="text-gray-700">
+            Люблю технологии, нестандартные задачи и запах свежесозданных проектов. 🚀
+          </p>
+
+          {/* Фото справа */}
+          <img
+            src="/profile.jpg"
+            alt="Евгений Кравцов"
+            className="w-32 h-32 rounded-lg justify-self-end"
+          />
+        </div>
 
         {/* Навыки */}
         <div className="mt-4">
@@ -39,27 +45,26 @@ export default function Home() {
           Есть идея? Пиши, обсудим. Люблю интересные проекты и креативные вызовы.</p>          
           </ul>
         </div>
-
-        {/* Кнопки */}
-        <div className="mt-6 flex space-x-4">
-          <a
-            href="https://t.me/rnbrk"
-            className="px-4 py-2 bg-black text-white rounded-lg"
-            target="_blank"
-            style={{ backgroundColor: "#01a3cf" }}
-            rel="noopener noreferrer"
-          >
-            Telegram канал
-          </a>
-          <a
-            href="https://t.me/nmtuiii"
-            className="px-4 py-2 bg-black text-white rounded-lg"
-            rel="noopener noreferrer"
-            style={{ backgroundColor: "#01a3cf" }}
-          >
-            Напиши
-          </a>
-        </div>
+{/* Кнопки */}
+<div className="mt-6 flex justify-center gap-4">
+  <a
+    href="https://t.me/rnbrk"
+    className="px-4 py-2 text-white rounded-lg"
+    target="_blank"
+    style={{ backgroundColor: "#01a3cf" }}
+    rel="noopener noreferrer"
+  >
+    Telegram канал
+  </a>
+  <a
+    href="https://t.me/nmtuiii"
+    className="px-4 py-2 text-white rounded-lg"
+    rel="noopener noreferrer"
+    style={{ backgroundColor: "#01a3cf" }}
+  >
+    Напиши
+  </a>
+</div>
       </div>
     </div>
   );
